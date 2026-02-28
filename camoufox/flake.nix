be4d2b46189e6201997cb70eb36966123e21ec0b
Alex Hp <drunkod@gmail.com>
2026-02-28 14:35:34 +0500

    feat: Refactor Camoufox build process to support multiple architectures and improve package management

--- camoufox/flake.nix
+++ camoufox/flake.nix
@@ -7,89 +7,121 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      version = "135.0.1-beta.24";
 
-      camoufox-pkg = pkgs.stdenv.mkDerivation rec {
-        pname = "camoufox";
-        version = "135.0.1-beta.24";
-
-        src = pkgs.fetchzip {
-          url = "https://github.com/daijro/camoufox/releases/download/v${version}/camoufox-${version}-lin.x86_64.zip";
+      # Per-system archive mapping: system -> { archSuffix, sha256 }
+      systemMap = {
+        "x86_64-linux" = {
+          archSuffix = "lin.x86_64";
           sha256 = "sha256-k5t12L5q0RG8Zun0SAjGthYQXUcf+xVHvk9Mknr97QY=";
-          stripRoot = false;
         };
-
-        nativeBuildInputs = [
-          pkgs.autoPatchelfHook
-          pkgs.wrapGAppsHook3
-          pkgs.lndir
-          pkgs.jq
-          pkgs.gtk3
-        ];
-
-        buildInputs = with pkgs; [
-          gtk3
-          glib
-          pango
-          cairo
-          gdk-pixbuf
-          atk
-          libxkbcommon
-          stdenv.cc.cc.lib
-          alsa-lib
-          gsettings-desktop-schemas
-          fontconfig
-          libglvnd
-          at-spi2-atk
-          dbus
-          librsvg
-          libx11
-          libxcomposite
-          libxdamage
-          libxfixes
-          libxrandr
-          libxrender
-          libxtst
-        ];
-
-        gappsWrapperArgs = [
-          "--prefix XDG_DATA_DIRS : ${pkgs.gsettings-desktop-schemas}/share"
-          "--prefix XDG_DATA_DIRS : ${pkgs.gtk3}/share"
-          "--prefix LD_LIBRARY_PATH : ${placeholder "out"}/lib/${pname}"
-        ];
-
-        installPhase = ''
-          runHook preInstall
-
-          mkdir -p $out/lib/${pname}
-          cp -r ./* $out/lib/${pname}/
-
-          mkdir -p $out/bin
-          ln -s $out/lib/${pname}/camoufox $out/bin/camoufox
-          ln -s $out/lib/${pname}/camoufox-bin $out/bin/camoufox-bin
-
-          runHook postInstall
-        '';
-
-        meta = with pkgs.lib; {
-          description = "A stealthy, minimalistic, custom build of Firefox for web scraping";
-          homepage = "https://github.com/daijro/camoufox";
-          license = licenses.mit;
-          platforms = platforms.linux;
-          mainProgram = "camoufox";
+        "aarch64-linux" = {
+          archSuffix = "lin.arm64";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+        "x86_64-darwin" = {
+          archSuffix = "mac.x86_64";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+        "aarch64-darwin" = {
+          archSuffix = "mac.arm64";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
         };
       };
+
+      supportedSystems = builtins.attrNames systemMap;
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems f;
+
+      mkCamoufox = system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          info = systemMap.${system};
+          isDarwin = pkgs.lib.hasSuffix "darwin" system;
+        in
+        pkgs.stdenv.mkDerivation rec {
+          pname = "camoufox";
+          inherit version;
+
+          src = pkgs.fetchzip {
+            url = "https://github.com/daijro/camoufox/releases/download/v${version}/camoufox-${version}-${info.archSuffix}.zip";
+            sha256 = info.sha256;
+            stripRoot = false;
+          };
+
+          nativeBuildInputs = [
+            pkgs.jq
+          ] ++ pkgs.lib.optionals (!isDarwin) [
+            pkgs.autoPatchelfHook
+            pkgs.wrapGAppsHook3
+            pkgs.lndir
+            pkgs.gtk3
+          ];
+
+          buildInputs = pkgs.lib.optionals (!isDarwin) (with pkgs; [
+            gtk3
+            glib
+            pango
+            cairo
+            gdk-pixbuf
+            atk
+            libxkbcommon
+            stdenv.cc.cc.lib
+            alsa-lib
+            gsettings-desktop-schemas
+            fontconfig
+            libglvnd
+            at-spi2-atk
+            dbus
+            librsvg
+            libx11
+            libxcomposite
+            libxdamage
+            libxfixes
+            libxrandr
+            libxrender
+            libxtst
+          ]);
+
+          gappsWrapperArgs = pkgs.lib.optionals (!isDarwin) [
+            "--prefix XDG_DATA_DIRS : ${pkgs.gsettings-desktop-schemas}/share"
+            "--prefix XDG_DATA_DIRS : ${pkgs.gtk3}/share"
+            "--prefix LD_LIBRARY_PATH : ${placeholder "out"}/lib/${pname}"
+          ];
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/lib/${pname}
+            cp -r ./* $out/lib/${pname}/
+
+            mkdir -p $out/bin
+            ln -s $out/lib/${pname}/camoufox $out/bin/camoufox
+            ln -s $out/lib/${pname}/camoufox-bin $out/bin/camoufox-bin
+
+            runHook postInstall
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A stealthy, minimalistic, custom build of Firefox for web scraping";
+            homepage = "https://github.com/daijro/camoufox";
+            license = licenses.mit;
+            platforms = supportedSystems;
+            mainProgram = "camoufox";
+          };
+        };
 
     in
     {
-      packages.${system} = {
-        camoufox = camoufox-pkg;
-        default = camoufox-pkg;
-      };
+      packages = forAllSystems (system: rec {
+        camoufox = mkCamoufox system;
+        default = camoufox;
+      });
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [ self.packages.${system}.camoufox ];
-      };
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          packages = [ self.packages.${system}.camoufox ];
+        };
+      });
     };
 }
